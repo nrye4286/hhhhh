7b5b6b2fad8bcac4adbf4bceac11378e01843b09
nrye4286 <91584544+nrye4286@users.noreply.github.com>
2023-11-04 22:40:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return render_template('./index.html')
+    return 'Hello World!'
+
+app.run(host = '0.0.0.0',port=5000,debug=True)
